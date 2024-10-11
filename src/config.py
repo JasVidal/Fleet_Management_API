fe0b054ca1f22d043444b0db2cd6132b22dbe778
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-10-10 21:03:36 -0500

    actualizar: modificando ubicación y creación de nuevos módulos

--- src/config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os #interactua con sistema operativo
-
-# Cargar las variables de entorno desde el archivo .env
-load_dotenv()
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
