b507f2564b3baf517f8955496671fb741e69d21f
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-10-24 14:04:45 -0500

    actualizar: actualizando nueva ubicación de archivo main app.py

---
+++ src/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from database.config import Config
+from routes.taxis_routes import *
+from routes.trajectories_routes import *
+from database.db import db
+
+
+
+#Se inicia la app Flask y SQLAlchemy
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+#Se inicializa SQLAlchemy
+db.init_app(app)
+
+init_routes_taxis(app)
+init_routes_trajectories(app)
+
+
+#Se importan las rutas
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
